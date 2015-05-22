4eb9ad7e82a41d6e5b0109340538b0c73c267ce9
Jooyung Han <jooyung.han@gmail.com>
2015-05-22 17:28:23 +0900

    pyramid example

--- thinkingfunctionallly/chap1.hs
+++ thinkingfunctionallly/chap1.hs
@@ -1,6 +1,29 @@
 
+fac 0 = 1
+fac n = fac (n-1) * n
+
+double n = n * 2
+square n = n * n
+
+squareOfDouble = square . double
+
+pyramid 0 = []
+pyramid n = pyramid (n-1) ++ [take' n (repeat' '*')]
+
+repeat' x = x : repeat' x
+
+take' 0 _ = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+len [] = 0
+len (_:xs)  = 1 + len xs 
+
+main :: IO () 
+main = do
+	(putStrLn . unlines . pyramid) 10
 
 
-main = putStrLn "Hello World"
+
 
 
