cbc97c1580ae3f7d0f7702623bdd63ed89a38a7d
Jooyung Han <jooyung.han@gmail.com>
2016-03-02 23:36:10 +0900

    Add some features to StateMonadPlus

--- uu-monad/smp.hs
+++ uu-monad/smp.hs
@@ -1,15 +1,24 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
 import Control.Monad
 import Data.Monoid
 import Data.Map
 import Data.List
+import Control.Monad.State
 
-newtype Diagnostics = Diagnostics {unD :: Map String Int} deriving Show
+type Stack a = [a]
+push :: a -> Stack a -> Stack a
+push = (:)
+top = head
+pop = tail
 
-instance Monoid Diagnostics where
-  mempty = Diagnostics (fromList []) 
-  mappend d1 d2 = Diagnostics $ unD d1 <> unD d2
+data Diagnostics s = Diagnostics {unD :: Map String Int, stack :: Stack s} deriving Show
 
-add label d = Diagnostics $ insertWith (+) label 1 $ unD d
+emptyD = Diagnostics empty []
+
+add :: String -> Diagnostics s -> Diagnostics s
+add label d = d {unD = insertWith (+) label 1 $ unD d}
 addBind = add "bind"
 addReturn = add "return"
 addDiagnostics = add "diagnostics"
@@ -17,11 +26,11 @@ addDiagnostics = add "diagnostics"
 showDiagnostics d = "[" ++ (intercalate ", " . fmap f . toList . unD $ d) ++ "]"
                     where f (s,n) = s ++ "=" ++ show n
 
-data StateMonadPlus s a = SMP {runSMP :: (Diagnostics,s) -> (a,Diagnostics,s)}
+data StateMonadPlus s a = SMP {runSMP :: (Diagnostics s, s) -> (Diagnostics s, Either String (a,s))}
 
 diagnostics :: StateMonadPlus s String
 diagnostics = SMP $ \(d,s) -> let d' = addDiagnostics d
-                              in (showDiagnostics d', d', s)
+                              in (d', Right (showDiagnostics d', s))
 
 annotate :: String -> StateMonadPlus s a -> StateMonadPlus s a
 annotate label m = SMP $ \(d,s) -> runSMP m (add label d, s)
@@ -34,9 +43,16 @@ instance Applicative (StateMonadPlus s) where
   (<*>) = ap
 
 instance Monad (StateMonadPlus s) where
-  return a = SMP $ \(d, s) -> (a, addReturn d, s)
-  m >>= f = SMP $ \(d, s) -> let (a, d', s') = runSMP m (addBind d, s)
-                             in runSMP (f a) (d', s')  
+  return a = SMP $ \(d, s) -> (addReturn d,  Right (a, s))
+  m >>= f = SMP $ \(d, s) -> let (d', result) = runSMP m (addBind d, s)
+                             in case result of
+                             Left err -> (d', Left err)
+                             Right (a, s') -> runSMP (f a) (d', s')
+  fail msg = SMP $ \(d, _) -> (add "fail" d, Left msg)
+
+instance MonadState s (StateMonadPlus s) where
+  get = SMP $ \(d,s) -> (add "get" d, Right (s, s))
+  put s = SMP $ \(d,_) -> (add "put" d, Right ((), s))
 
 e1 = do return 3 >> return 4
         return 5
@@ -45,3 +61,25 @@ e1 = do return 3 >> return 4
 e2 = do annotate "A" (return 3 >> return 4)
         return 5
         diagnostics
+
+runStateMonadPlus :: StateMonadPlus s a -> s -> Either String (a, s)
+runStateMonadPlus m s = snd $ runSMP m (emptyD, s)
+
+
+class MonadState s m => StoreState s m|m->s where
+  saveState :: m ()
+  loadState :: m ()
+
+e3 :: (Num s, StoreState s m) => m (s,s,s,s,s)
+e3 = do i1<-get;saveState
+        modify (*2)
+        i2<-get;saveState
+        modify (*2)
+        i3<-get;loadState
+        i4<-get;loadState
+        i5<-get
+        return (i1,i2,i3,i4,i5)
+
+instance StoreState s (StateMonadPlus s) where
+  saveState = SMP $ \(d,s) -> (d {stack = push s (stack d)}, Right((),s))
+  loadState = SMP $ \(d,s) -> (d {stack = pop (stack d)}, Right((), top (stack d)))
